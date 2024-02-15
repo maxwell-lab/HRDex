530677a8a41632f2d37d716e9282fb5d97ee353a
Brad Wubbenhorst <bwubbenhorst@gmail.com>
2024-02-14 19:11:38 -0500

    Update getCNT.R

--- R/getCNT.R
+++ R/getCNT.R
@@ -1,17 +1,17 @@
 #' get the main copynumber state for each chromosome
-#' 
+#'
 #' @param seq.dat the data.frame of sequencing data
-#' 
+#'
 #' @details Get the main copynumber state for each chromosome. Calculate the total proportion of
 #' the chromosome associated with each unique copynumber state; return the copynumber state that is most
 #' represented.
-#' 
+#'
 #' @return a data.frame containing the main copynumber state for each input chromosome
-#' 
-#' @examples 
+#'
+#' @examples
 #' seq.dat <- preprocessHRD( seq.dat )
 #' CN.dat <- getCNt( seq.dat )
-#' 
+#'
 #' @export
 
 
@@ -24,35 +24,44 @@ getCNt <- function( seq.dat )
   #   CN.dat (data.frame), then number
 {
   main.CN <- rep(0, length(unique(seq.dat$chromosome)))
-  
+
   ct1 <- 0
   for( chr in unique(seq.dat$chromosome))
   {
     ct1 <- ct1 + 1
-    
+
     # reduce data to one chromosome at a time
     dat <- seq.dat[seq.dat$chromosome == chr,]
-    
+
     # remove any NAs or this will crash
     dat <- dat[!is.na(dat$CNt),]
     CNt.frac <- rep(0, length(unique(dat$CNt)))
     ct2 <- 0
-    
+
     # get the fraction of the chromosome association with each copynumber state
     for( CNt in unique(dat$CNt) )
     {
       ct2 <- ct2 + 1
       CNt.frac[ct2] <- sum(dat$frac.chr[dat$CNt == CNt])
     }
-    
+
     # record values + associated CNt
     out <- data.frame(CNt=unique(dat$CNt), CNt.frac=CNt.frac)
-    
+
     # get copynumber state most prominent in the chromosome
-    main.CN[ct1] <- out$CNt[which(CNt.frac == max(CNt.frac))]
-    
+    if(length(CNt.frac) > 0 && any(!is.na(CNt.frac))) {
+      max_frac_indices <- which(CNt.frac == max(CNt.frac, na.rm = TRUE))
+      if(length(max_frac_indices) > 0) {
+        main.CN[ct1] <- out$CNt[max_frac_indices[1]]  # Use the first if there are multiple maxima
+      } else {
+        main.CN[ct1] <- NA  # Or handle differently if no maximum fraction is found
+      }
+    } else {
+      main.CN[ct1] <- NA  # Or set a default value or handle as appropriate
+    }
+
   }
-  
+
   CN.dat <- data.frame(chromosome=unique(seq.dat$chromosome), main.CN=main.CN)
   return(CN.dat)
 }
